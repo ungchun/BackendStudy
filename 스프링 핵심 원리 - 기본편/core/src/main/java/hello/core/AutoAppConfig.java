405c6e55fb74b329fb24f546dc1647a94c5092c8
sunghun <leedool3003@gmail.com>
2023-04-07 15:30:44 +0900

    컴포넌트 스캔

---
+++ 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,25 @@
+package hello.core;
+
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        // excludeFilters 자동등록 제외시켜줌
+        // @Configuration은 자동등록 제외
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+
+//        basePackages = "hello.core.member", // 여기서 부터 찾아서 등록하라는 뜻
+//        basePackageClasses = AutoAppConfig.class
+)
+public class AutoAppConfig {
+
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
