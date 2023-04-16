7d210f91d4b022d7c232d85c619306a4b312d19b
sunghun <leedool3003@gmail.com>
2023-04-16 15:04:43 +0900

    스프링 MVC - 구조 이해

--- 스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술/src/main/java/hello/servlet/ServletApplication.java
+++ 스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술/src/main/java/hello/servlet/ServletApplication.java
@@ -1,8 +1,11 @@
 package hello.servlet;
 
+import hello.servlet.web.springmvc.v1.SpringMemberFormControllerV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan // 서블릿 자동 등록
 @SpringBootApplication
