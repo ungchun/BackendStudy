53a1557bac8ae3a77dac599fec11ba635bff9895
sunghun <leedool3003@gmail.com>
2023-04-30 17:33:04 +0900

    섹션 10. 스프링 타입 컨버터

---
+++ 스프링 MVC 2편 - 백엔드 웹 개발 활용 기술 - 7/src/main/java/hello/typeconverter/converter/IpPortToStringConverter.java
@@ -0,0 +1,13 @@
+package hello.typeconverter.converter;
+
+import hello.typeconverter.type.IpPort;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.convert.converter.Converter;
+
+@Slf4j
+public class IpPortToStringConverter implements Converter<IpPort, String> {
+    @Override
+    public String convert(IpPort source) {
+        return source.getIp() + ":" + source.getPort();
+    }
+}
---
+++ 스프링 MVC 2편 - 백엔드 웹 개발 활용 기술 - 7/src/test/java/hello/typeconverter/converter/ConversionServiceTest.java
@@ -0,0 +1,23 @@
+package hello.typeconverter.converter;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.convert.support.DefaultConversionService;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ConversionServiceTest {
+
+    @Test
+    void conversionService() {
+        // 등록
+        DefaultConversionService conversionService = new DefaultConversionService();
+        conversionService.addConverter(new StringToIntegerConverter());
+        conversionService.addConverter(new IntegerToStringConverter());
+        conversionService.addConverter(new StringToIpPortConverter());
+        conversionService.addConverter(new IpPortToStringConverter());
+
+        // 사용
+        assertThat(conversionService.convert("10", Integer.class)).isEqualTo(10);
+    }
+}
