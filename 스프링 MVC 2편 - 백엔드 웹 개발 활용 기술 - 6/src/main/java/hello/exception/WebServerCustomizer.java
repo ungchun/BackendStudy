e213755555adc29708f1e12b8c27dc064f5e7da5
sunghun <leedool3003@gmail.com>
2023-04-30 15:05:00 +0900

    섹션 9.API 예외 처리

--- 스프링 MVC 2편 - 백엔드 웹 개발 활용 기술 - 6/src/main/java/hello/exception/WebServerCustomizer.java
+++ 스프링 MVC 2편 - 백엔드 웹 개발 활용 기술 - 6/src/main/java/hello/exception/WebServerCustomizer.java
@@ -6,6 +6,8 @@ import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
+
+
 //@Component
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
     @Override
