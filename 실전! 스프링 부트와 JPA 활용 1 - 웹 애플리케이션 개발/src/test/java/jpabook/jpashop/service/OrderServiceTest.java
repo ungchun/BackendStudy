697b33855b092d54822bb4b5e311dd169dd06e06
sunghun <leedool3003@gmail.com>
2023-05-07 17:26:04 +0900

    섹션 6. 주문 도메인 개발

---
+++ 실전! 스프링 부트와 JPA 활용 1 - 웹 애플리케이션 개발/src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -0,0 +1,75 @@
+package jpabook.jpashop.service;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Item.Book;
+import jpabook.jpashop.domain.Item.Item;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.repository.OrderRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class OrderServiceTest {
+
+    @Autowired
+    EntityManager em;
+    @Autowired
+    OrderService orderService;
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    public void 상품주문() throws Exception {
+        // given
+        Member member = new Member();
+        member.setName("회원1");
+        member.setAddress(new Address("서울", "경기", "123-123"));
+        em.persist(member);
+
+        Book book = new Book();
+        book.setName("시골 JPA");
+        book.setPrice(10000);
+        book.setStockQuantity(10);
+        em.persist(book);
+
+        int orderCount = 2;
+
+        // when
+        Long orderId = orderService.order(member.getId(), book.getId(), orderCount);
+
+        // then
+        Order getOrder = orderRepository.findOne(orderId);
+
+        assertThat(OrderStatus.ORDER).isEqualTo(getOrder.getStatus());
+        assertThat(1).isEqualTo(getOrder.getOrderItems().size());
+        assertThat(10000 * orderCount).isEqualTo(getOrder.getTotalPrice());
+        assertThat(8).isEqualTo(book.getStockQuantity());
+    }
+
+    @Test
+    public void 주문취소() throws Exception {
+        // given
+
+        // when
+
+        // then
+    }
+    @Test
+    public void 상품주문_재고수량초과() throws Exception {
+        // given
+
+        // when
+
+        // then
+    }
+}
