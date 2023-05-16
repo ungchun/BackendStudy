41a7552f3f744ba3933842f3c4875f7ee1226f9c
sunghun <leedool3003@gmail.com>
2023-05-16 23:55:30 +0900

    섹션 8. 프록시와 연관관계 관리

--- jpashop/src/main/java/domain/Item.java
+++ jpashop/src/main/java/domain/Item.java
@@ -6,7 +6,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Item {
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+public abstract class Item {
     @Id
     @GeneratedValue
     @Column(name = "ITEM_ID")
