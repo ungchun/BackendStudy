da5370e34deb4a5fb773102307ceaed48e975ee3
sunghun <leedool3003@gmail.com>
2023-06-08 20:57:44 +0900

    섹션 3. 공통 인터페이스 기능

--- data-jpa/src/main/java/study/datajpa/DataJpaApplication.java
+++ data-jpa/src/main/java/study/datajpa/DataJpaApplication.java
@@ -2,6 +2,7 @@ package study.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class DataJpaApplication {
@@ -11,3 +12,4 @@ public class DataJpaApplication {
 	}
 
 }
+
