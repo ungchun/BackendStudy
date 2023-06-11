750827fd9efe6b9161cf59b54cd3b780ffca0202
sunghun <leedool3003@gmail.com>
2023-06-11 16:39:50 +0900

    섹션 6. 스프링 데이터 JPA 분석

---
+++ data-jpa/src/main/java/study/datajpa/entity/Item.java
@@ -0,0 +1,37 @@
+package study.datajpa.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Item implements Persistable<String> {
+
+    @Id
+    private String id;
+
+    public Item(String id) {
+        this.id = id;
+    }
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public boolean isNew() {
+        return createdDate == null;
+    }
+}
